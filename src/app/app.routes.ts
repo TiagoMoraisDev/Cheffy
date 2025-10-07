e4b6de0d04806d1cdc774de002745634560705ed
TiagoMoraisDev <tiagomoraispro@gmail.com>
2025-10-07 16:12:42 -0300

    feat: add routing for main application pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/painel/painel.component').then((m) => m.PainelComponent),
+  },
+  {
+    path: 'historico',
+    loadComponent: () =>
+      import('./pages/historico/historico.component').then(
+        (m) => m.HistoricoComponent
+      ),
+  },
+  {
+    path: 'gerenciar-menu',
+    loadComponent: () =>
+      import('./pages/gerenciar-menu/gerenciar-menu.component').then(
+        (m) => m.GerenciarMenuComponent
+      ),
+  },
+  {
+    path: 'menu',
+    loadComponent: () =>
+      import('./pages/menu/menu.component').then((m) => m.MenuComponent),
+  },
+];
